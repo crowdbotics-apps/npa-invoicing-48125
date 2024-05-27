5cd153a271b8e794430edf13d58e74df39241803
Crowdbotics <team@crowdbotics.com>
2024-05-27 20:07:54 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "npa-invoicing-48125",
+  url: "https://npa-invoicing-48125.botics.co",
+  api: "https://npa-invoicing-48125.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
